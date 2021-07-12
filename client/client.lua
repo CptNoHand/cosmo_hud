0bb2c777a4fe877af4a0e68caaa94b93d41d4c71
izMystic <mystic@izmystic.dev>
2021-07-12 09:54:30 -0400

    GetObject

--- client/client.lua
+++ client/client.lua
@@ -6,14 +6,6 @@ local hunger = nil
 local thirst = nil
 local oxygen = 0
 
-QBCore = nil
-Citizen.CreateThread(function()
-    while QBCore == nil do
-        TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-        Wait(200)
-    end
-end)
-
 RegisterNetEvent("QBCore:Client:OnPlayerLoaded")
 AddEventHandler("QBCore:Client:OnPlayerLoaded", function()
     isLoggedIn = true
